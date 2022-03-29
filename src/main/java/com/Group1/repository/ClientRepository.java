72944b579c84a0d0cb70e2ff49e40dc33e05bfc0
VladislavMislivskii <74928371+VladislavMislivskii@users.noreply.github.com>
2022-03-29 14:18:29 +0300

    Revert "BaseStart"
    This reverts commit 3bfa82e6e41a18f37410960d974eb0bea30364bb.

--- src/main/java/com/Group1/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Group1.repository;
-
-import com.Group1.entity.Client; //Подключаем объект
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository  extends CrudRepository<Client, String> { //указываем, что мы будем манипулировать с данными
-}
